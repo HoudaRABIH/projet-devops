f25146acade442a227ad9191379647b54f6ac026
Meryemel21 <meryem.elfaize@etu.uae.ac.ma mkdir projetcd projetgit init>
2026-04-25 19:07:53 +0100

    Frontend Angular - boutique sacs et ceintures en cuir

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,26 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+@Component({ selector: 'app-root', standalone: true, imports: [CommonModule, HttpClientModule], templateUrl: './app.html', styleUrls: ['./app.css'] })
+export class App implements OnInit {
+  cartOpen = false;
+  cartItems: any[] = [];
+  products: any[] = [];
 
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
-})
-export class App {
-  protected readonly title = signal('frontend');
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('http://localhost:8000/api/products')
+      .subscribe({
+        next: (data: any) => {
+          this.products = data;
+        },
+        error: (err) => {
+          console.error('Erreur API:', err);
+        }
+      });
+  }
+
+  addToCart(p: any){ this.cartItems.push(p); this.cartOpen=true; }
+  getTotal(){ return this.cartItems.reduce((s,i)=>s+i.price,0); }
 }
